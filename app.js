cfc3b2c2c8eaafaf822d7442b6ab39001234409e
keinnerross <keinnerross@gmail.com>
2025-02-01 19:28:33 -0300

    setting

--- app.js
+++ app.js
@@ -5,43 +5,28 @@ const client = new Client({
     authStrategy: new LocalAuth(),
 });
 
-// Generar QR para iniciar sesión
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
-});
-
 client.on('ready', async () => {
-    console.log('Cliente listo para enviar mensajes!');
+
+
+    const chats = await client.getChats();
 
     try {
-        // Obtener todos los chats
-        const chats = await client.getChats();
-
-        // Filtrar solo los grupos basándonos en el servidor del ID
         const groups = chats.filter(chat => chat.id.server === 'g.us');
-
-        // Mostrar los nombres de los grupos en consola
         console.log('Lista de grupos disponibles:');
         groups.forEach(group => {
             console.log(`- ${group.name} (ID: ${group.id._serialized})`);
         });
 
-        const groupName = 'DarkWolf Team🐺'; 
-        const group = groups.find(chat => chat.name === groupName);
-
-        if (group) {
-            await client.sendMessage(group.id._serialized, 'Test de Link: https://keinnerross.github.io/portfolioross/');
-            console.log('Mensaje enviado al grupo:', group.name);
-        } else {
-            console.log(`Grupo con nombre "${groupName}" no encontrado.`);
-        }
     } catch (error) {
-        console.error('Error al enviar mensaje:', error);
+        console.log('Error al obtener los grupos');
+        console.error(error);
     }
 });
 
-client.on('message', message => {
-    console.log(`Mensaje recibido: ${message.body}`);
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
 });
+
+
 
 client.initialize();
